ad7388522168a40e17a64ba14dbd4a9e60f16806
Blockchain Dev <nevillebrown_930@hotmail.com>
2021-02-23 19:02:35 -0500

    Level 2

--- app.js
+++ app.js
@@ -21,7 +21,7 @@ const userSchema = new mongoose.Schema ({
   email: String,
   password: String
 });
-
+ //
 
 userSchema.plugin(encrypt, { secret: process.env.SECRET, encryptedFields: ['password']});
 
